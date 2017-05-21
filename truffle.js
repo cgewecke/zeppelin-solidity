25cf8a8c7f4c0d48dcb6c141b42fda03fe80eeeb
cgewecke <christophergewecke@gmail.com>
2017-05-21 10:16:17 -0700

    Add solidity-coverage

--- truffle.js
+++ truffle.js
@@ -1,11 +1,13 @@
 require('babel-register');
 require('babel-polyfill');
 
+var provider;
 var HDWalletProvider = require('truffle-hdwallet-provider');
-
 var mnemonic = '[REDACTED]';
-var provider = new HDWalletProvider(mnemonic, 'https://ropsten.infura.io/');
 
+if (!process.env.SOLIDITY_COVERAGE){
+  provider = new HDWalletProvider(mnemonic, 'https://ropsten.infura.io/')
+}
 
 module.exports = {
   networks: {
@@ -17,6 +19,13 @@ module.exports = {
     ropsten: {
       provider: provider,
       network_id: 3 // official id of the ropsten network
+    },
+    coverage: {
+      host: "localhost",
+      network_id: "*", 
+      port: 8555,         
+      gas: 0xfffffffffff, 
+      gasPrice: 0x01      
     }
   }
 };
